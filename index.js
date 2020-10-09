52c756f1224d32d339a68468ffe4d11acfc31437
JD <janelle.m.deane@gmail.com>
2020-10-08 19:04:47 -0700

    debugged server

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 // ==============================================================================
 var path = require("path"); 
 var express = require("express");
-var fs = require("fs")
+var fs = require("fs");
+
 
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -15,6 +16,10 @@ var app = express();
 // Sets an initial port and compatiable with Heroku 
 var PORT = process.env.PORT || 8080;
 
+// Sets up the linking to add & view js
+// =============================================================
+app.use(express.static("public"))
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -25,32 +30,43 @@ app.use(express.json());
 
 // Returns index.html file
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
   });
 
 // GET *  Return the index.html file
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
   });
 
   
 //   Returns notes.html file
   app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
   
   // GET /api/notes - Should read the db.json file and return all saved notes as JSON.
   // Displays the db.json and returns the saved all notes
   app.get("/api/notes", function(req, res) {
-    return res.json(NOTES);
+    let dbData= fs.readFileSync("db/db.json", "utf-8");
+    dbData=JSON.parse(dbData)
+    return res.json(dbData);
   });
-
-
-
-
 
 // POST /api/notes - Should receive a new note to save on the request body, add it 
 // to the db.json file, and then return the new note to the client.
+app.post("/api/notes", function(req, res) {
+  let dbData = fs.readFileSync("db/db.json", "utf-8");
+  dbData = JSON.parse(dbData);
+  let newDbData = {
+  "title": req.body.title,
+  "text": req.body.text,
+  "id": Date.now(),
+  }
+  dbData.push(newDbData);
+  fs.writeFileSync(path.join(__dirname, "db/db.json"), JSON.stringify(dbData, null, 2));
+  res.send(true);
+})
+
 
 // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. 
 // This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, 
